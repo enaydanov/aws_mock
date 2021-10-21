1155588aa85c21ed91f1242799a31561a84d780b
Fabio Gelcer <fabio@scylladb.com>
2021-10-21 23:21:19 +0300

    feature(response): add response for s3 get requests
    added the 2 S3 requests we got for the creation of
    the SCT RUNNER.

--- aws_mock/main.py
+++ aws_mock/main.py
@@ -1,3 +1,5 @@
+from aws_mock.responses import scylla_qa_ec2_key
+
 from flask import Flask
 
 
@@ -7,3 +9,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Nginx, uWSGI, and Flask"
+
+
+@app.route("/scylla-qa-ec2.pub")
+def return_scylla_qa_ec2_public_key():
+    return scylla_qa_ec2_key.get_public_key()
+
+
+@app.route("/scylla-qa-ec2")
+def return_scylla_qa_ec2_private_key():
+    return scylla_qa_ec2_key.get_private_key()
