de97ecc6cd0b8d54f61cd4295043f3e15f009481
Fabio Gelcer <fabio@scylladb.com>
2021-10-25 14:49:19 +0300

    fix(ssh key pair): using now dedent to organise it
    and also, using now a real key pair, that can be
    useful for a real "ssh" connection, to mock also
    SSH requests, to a "fake" ssh server.

--- aws_mock/responses/scylla_qa_ec2_key.py
+++ aws_mock/responses/scylla_qa_ec2_key.py
@@ -1,114 +1,69 @@
 import datetime
+from textwrap import dedent
 
 from flask import Response
 
 
-PUBLIC_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQD8wI17u0/sHGyOXL4Cb/Ml2oL1pVjyj5NLHI0eImwZ1Vddy8K8rrrkwaZs6w3" \
-             "q4HYqXp86LYI8o4ZjydBkaw7pe7i9evMZ7Xc4GzqIW7Yx5dh7yA3zRY61hR7NjbJ+demKW9GLnydByHSgiv1AlbNvneKpxN7or2" \
-             "CsK51dVXgn+R2yMqv+RRHMl4W9tvkC2HZRCR/k+TgUGvKoN+UOZ1l5P7HDi5T3IxS4PpN/fEBGngsyU97nF+5dO2kCSy8MQT8wU" \
-             "9O45hJSd1RO7476hITbL4LED8fjIP3g8L43s7zz098Epk1sKl7+cXY2aypwFCG/8YkgE8M625pdk1KC4wFrPogbZByD3v0JVQUA" \
-             "eMAVqy63i/oSSocC3gqkanfFnEaaTW0aKoUqgYQS0/dsVx8vWenVFEukpz1n41AQcD7Vgk2XUp1UDvbKI2g4SGA8AB3awOKKK9y" \
-             "njAbvQEV1sLZo0b4NKY6P1vWY+EMhevwo6RjDC9UB2Ic11sllmiaoAXo4zDq1X/RUPzx8nZGTRULKE5egEhQnasGa38GAO5nIU6" \
-             "wIpB0VwYZsRjctTPnUkcIe3cnFt5DLIZaXjP0vogTZOOwG+/DU55c5MxCmCzCBk1C0T/UGkOekfXXWrtAWzgbcJf1EKKuxX9QWY" \
-             "cdojXsHx/ZWphGAmWpZlcRF0mERdw==scylla-qa-ec2"
-PRIVATE_KEY = "-----BEGIN OPENSSH PRIVATE KEY-----" \
-              "" \
-              "b3BlbnNzac1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAACFwAAAAdzc2gtcn" \
-              "" \
-              "NhAAAAAwEaAQAAAgEA/MCNe7vP7Bxsjly+Am/zJdqC9aVY8o+TSxyNHiJsGdlXXcvCvK66" \
-              "" \
-              "5MGj7OsN6aB2Kl6fOi2CPKOGY8nQZClO6Xu4vXrzGe13OBs6iFu2MeXYe8gN80WOtYUezY" \
-              "" \
-              "2yfnXpilvSi58nQch0oIr9QJWzb53iqcTe6K9grCudXVV4J/kdsjKr/kURzJeFvbb5Ath2" \
-              "" \
-              "UQkf5Pk4FVryqDflDmdZeT+xw4uU9yMUuD6Tf3xARp4LMlPe5xfuXTtpAksvDEE/MFPTuO" \
-              "" \
-              "YSUndUTu+3+oSE2y+CxA/H4yD94PC+N7O889PfBKZNbCpe/nF2NmsqcBQhv/GJIBPDOtua" \
-              "" \
-              "XZNSguMBaA6IG2Qcg979CVUFAHjAFasut4v6EkqHAt4KpGp3xZxGmk1tGiqFKoGEEtP3bF" \
-              "" \
-              "cfL1np1RRApKc9Z+NQEHA+1YJNl1KdVA72yiNoOEhgPAAd2sDiiivcp4wG70BFdbC2aNG+" \
-              "" \
-              "DSmOj9b1mahDIXr8KOkYwwvVAdiHNdbJZZomqAF6OMw6tV/0VD88fJ2Rk0VCyhOXoBIUJ2" \
-              "" \
-              "rBmt/BgDuAyFOsCKQdFcGGbEY3LUz51JHCHt3JxbeQyyGWl4z9L6IE2TjsBvvw1OeXOTMQ" \
-              "" \
-              "pgswgZNQtW/1BpDnpH111q7QFs4G3CX9RCirsV/UFmHHaI17B8f2VqYRgJlqWZXERdJhEX" \
-              "" \
-              "cAAAdIfIg1QnyIPUIAAAAHc3NoLXJzYQAAAgEA/MCNe7vP7Bxsjly+Am/zJdqC9aVY8o+T" \
-              "" \
-              "SxyNHiJsRdlXXcvCvK665MGj7OsN6uB2Kl6fOi2CPKOGY8nQZClO6Xu4vXrzGe13OBs6iF" \
-              "" \
-              "u2MeXYe8fN80WOtYUezY2yfnXpilvRi58nQch0oIr9QJWzb53iqcTe6K9grCudXVV4J/kd" \
-              "" \
-              "sjKr/kUQzJeFvbb5Ath2UQkf5Pk4FBryqDflDmdZeT+xw4uU9yMUuD6Tf3xARp4LMlPe5x" \
-              "" \
-              "fuXTtpaksvDEE/MFPTuOYSUndUTu+O+oSE2y+CxA/H4yD94PC+N7O889PfBKZNbCpe/nF2" \
-              "" \
-              "NmsqcbQhv/GJIBPDOtuaXZNSguMBaz6IG2Qcg979CVUFAHjAFasut4v6EkqHAt4KpGp3xZ" \
-              "" \
-              "xGmk0tGiqFKoGEEtP3bFcfL1np1RRLpKc9Z+NQEHA+1YJNl1KdVA72yiNoOEhgPAAd2sDi" \
-              "" \
-              "iivqp4wG70BFdbC2aNG+DSmOj9b1mPhDIXr8KOkYwwvVAdiHNdbJZZomqAF6OMw6tV/0VD" \
-              "" \
-              "88qJ2Rk0VCyhOXoBIUJ2rBmt/BgDuZyFOsCKQdFcGGbEY3LUz51JHCHt3JxbeQyyGWl4z9" \
-              "" \
-              "L5IE2TjsBavw1OeXOTMQpgswgZNQtE/1BpDnpH111q7QFs4G3CX9RCirsV/UFmHHaI17B8" \
-              "" \
-              "a2VqYRgJl1WZXERdJhEXcAAAADAQABAAACAQCClOzD53V1s/21015rnfEONoWAagu5Tco4" \
-              "" \
-              "zSi/T+Nvu5Uy1Wnn15JBS3XomkwXdB44p6g2A3sM3yM4fPTIOrtFT6FTa6kbVR9KAm9MMa" \
-              "" \
-              "3Au9ILqYDjlaU0TPFsG3ADe+HLlMJ/ExvSFcGDjCtpaa5J1VVchpTvvGQCr4nexaCNblwQ" \
-              "" \
-              "S6+mb4LRo9r3+AVQngsKtN8gik2ZvY5usGoL9gM47Fpn/75XrG0w/0tvCv+ePFrmhoRb+n" \
-              "" \
-              "Qw/tey0qsc9ryxuC1jkqIWrwYEvimtbjfBkhtKNij8RHhF9bsgxiCyDF1lZpILdxya8n2z" \
-              "" \
-              "pcxsGUqSgzRoOS2dRAXNoKZ5btS0z8Obh51Ce9zTBZJX0hZC0EMX5vgTrEH1hOut8wreQ3" \
-              "" \
-              "pz0WN2eBrj2slJgYJFJ2WUa2qzmljR1aYkQ7IkUQqZthA5I2OQMXtn07lNppz3BZEC0KGK" \
-              "" \
-              "dmzhhRrKvoq90zYq7aOQ39x8nINxv8ia8zb65RLnBgWInLwx0dIiUiQk2/q5DarI1/UV1u" \
-              "" \
-              "L/BR2vdgq7kllbyQ3M/cZayFj6G/OikaDA2GPf3q4NHSr3TG6SS5BloH+ZizmwfxUh+r60" \
-              "" \
-              "/NISh3vVGIpeF8CHQuNdK6iCA3JpnO9RIuRAEQ6pSCh5OdRlDNGNF+PpoaRKHBHkqMAloN" \
-              "" \
-              "qmdJ0Va+s3FAnvFHmcQQAAAQBvFW8rBL6pl/UEsl4esoPhIavtM0fDLU78aAlkv4J7Ys5J" \
-              "" \
-              "9wFMGLFrAqUSTIrCKGfig8E2pcM7L95mDbF48L/VfKAhQbn6XRWRltCAvfgWR9svh7/9M8" \
-              "" \
-              "myhQVgHt+3bsNed8njQ/QPWNWbHrNH0RIUX8xSvPG6pIrdNW+r3PqFCHUHp2Ol23wZaJOt" \
-              "" \
-              "XIY4jMMfS3UcUNw0zvh4taa8efrR1/3kfXPXk/kUvFY6YgBXxuXE1HdlI4v+Hbq2YoVXJT" \
-              "" \
-              "N30iXSYAYLz/+LfTfb3tMryUBcBp7g5xMGdOduBwerkD0GVUFi0GpRbWjmL+FGK5p2aFEW" \
-              "" \
-              "temcM370zbyxazmHAAABAQD+uBxNWHL0uKLm/Y2hTrTNmt4cA8n4kebNWmv2KfCFApFVKI" \
-              "" \
-              "X/h09cnzK2fcqfI+Ch1IVyhYrgDqnaNXz35rrKfauWyanjTvl8qf8EGJyxYjKrtfmOHh54" \
-              "" \
-              "3gkyXZZP7MI+b+0uokGthXZMC0tL5e0dbkFsQcAi5CUBSLdER1an0LFFdG+TmSjRQxfvZL" \
-              "" \
-              "ZPxt4emc2oGIZb5SiaYgGmp2NxwOTWgJAoFYw+AsLmSzTTRdpqjl+CU6vrjJi48ER6cZNp" \
-              "" \
-              "rXMK6z5OBfQ/cpOS7zTTohP9qsCZmpeNfrSycvUfrPq7QQ8dEu5wd4fQZLke0n2X9FHB36" \
-              "" \
-              "pw6JlWFY0rn5ohAAABAQD+Bej41Zs3OdS+9t9Xx1pEwpDr68SniHKup2nIaVA+4mpxSyNc" \
-              "" \
-              "RZm3Nzvz+7t9AIxqOqdOY/fXf7gOx6oiyYKbAZQTvPSRcQzRpkDT/2tzqNmdsjdB40Z3a7" \
-              "" \
-              "TsA8Te0XPyTH0nkw/gUzLKYgUq+1ydz7/8ML4esf574K/BOWGx9Jq4ZaRmiSu/UlufKH0X" \
-              "" \
-              "KIol5hnQUab4UeIwDbDqFUC5lFLUfmulier+VlwlY2lbxE4PhsefSMhgndSj4hiwDs3VrQ" \
-              "" \
-              "lMASzx311JXfD3wySxMUaeLZZT7M1jBQTyPeUnot+Cj+q6aN+QPXn6K43hWV4+jX1/rYH7" \
-              "" \
-              "9DiTkbHSKCiXAAAAEmZydWNoQENjeWxsYWRiLmNvbQ==" \
-              "" \
-              "-----END OPENSSH PRIVATE KEY-----" \
-              ""
+PUBLIC_KEY = dedent(
+    """ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCn/ong1NEz4cQCHf8TjzRXD5DUQ4McFBXpQj0TN8ART6LF3jVeDYSZlYRlnDMQvMfZkNJWlDCa
+    knS5fWQzp50/r/7ACw2RI/aFpZHshGMm2iLXaLMyVqeW5T3pWpU5nr5j9hzdVqv1zuzhq1qE3r/yRM3nVIotH+6S87ZmFhISeMgNbhC2AFH3O9ntt+B
+    iQ1uAGJkozg3pxhdF0GI0d3zQNv4P0krChqHSf355YyAVzBpQk2t+8Y1fmJM6bX+bmIRvvsXxCGjqbdoLGrJHmdWwej8K8W0Ov9xUwg8pWbji5+dmCP
+    dWUQPaAemefVql9xWHPnc2AGVe4E2H5OmJRnpwND84QLCHpvXyyyipnaI+a4tKjPBwp3oSA4pOwbdwh/f6/l73Rl/FO9k2N4Uz651gVNuL6IxP79t9H
+    PMTsYkTnXGG3WrjPMZ5za7u/CjtQ+DXd1nRGxqbmlybtbXooPb2TPybs08R3vTpsXx+x3HgpBncTiWBGkl6Wm/X0zLyGksGG/9sgnSD3d4n9d/qEg8W
+    M+Nptw4vQjzI4pI24d0sAAlVrM3hqDq7KsiT2TgepiwdnQuMtL2I05CzjviQzBVlr0lSkoui86POlRkBMmjpfzxeGIhCMagpB/0BkkF0kqNWJBE1OVE
+    EjJUAoV88l5SyhvOV6PAal31GgtKw7qyj3Q== qa-bot@scylladb.com""")
+
+PRIVATE_KEY = dedent(
+    """-----BEGIN OPENSSH PRIVATE KEY-----
+    b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAACFwAAAAdzc2gtcn
+    NhAAAAAwEAAQAAAgEAp/6J4NTRM+HEAh3/E480Vw+Q1EODHBQV6UI9EzfAEU+ixd41Xg2E
+    mZWEZZwzELzH2ZDSVpQwmpJ0uX1kM6edP6/+wAsNkSP2haWR7IRjJtoi12izMlanluU96V
+    qVOZ6+Y/Yc3Var9c7s4atahN6/8kTN51SKLR/ukvO2ZhYSEnjIDW4QtgBR9zvZ7bfgYkNb
+    gBiZKM4N6cYXRdBiNHd80Db+D9JKwoah0n9+eWMgFcwaUJNrfvGNX5iTOm1/m5iEb77F8Q
+    ho6m3aCxqyR5nVsHo/CvFtDr/cVMIPKVm44ufnZgj3VlED2gHpnn1apfcVhz53NgBlXuBN
+    h+TpiUZ6cDQ/OECwh6b18ssoqZ2iPmuLSozwcKd6EgOKTsG3cIf3+v5e90ZfxTvZNjeFM+
+    udYFTbi+iMT+/bfRzzE7GJE51xht1q4zzGec2u7vwo7UPg13dZ0Rsam5pcm7W16KD29kz8
+    m7NPEd706bF8fsdx4KQZ3E4lgRpJelpv19My8hpLBhv/bIJ0g93eJ/Xf6hIPFjPjabcOL0
+    I8yOKSNuHdLAAJVazN4ag6uyrIk9k4HqYsHZ0LjLS9iNOQs474kMwVZa9JUpKLovOjzpUZ
+    ATJo6X88XhiIQjGoKQf9AZJBdJKjViQRNTlRBIyVAKFfPJeUsobzlejwGpd9RoLSsO6so9
+    0AAAdQ3HZOTdx2Tk0AAAAHc3NoLXJzYQAAAgEAp/6J4NTRM+HEAh3/E480Vw+Q1EODHBQV
+    6UI9EzfAEU+ixd41Xg2EmZWEZZwzELzH2ZDSVpQwmpJ0uX1kM6edP6/+wAsNkSP2haWR7I
+    RjJtoi12izMlanluU96VqVOZ6+Y/Yc3Var9c7s4atahN6/8kTN51SKLR/ukvO2ZhYSEnjI
+    DW4QtgBR9zvZ7bfgYkNbgBiZKM4N6cYXRdBiNHd80Db+D9JKwoah0n9+eWMgFcwaUJNrfv
+    GNX5iTOm1/m5iEb77F8Qho6m3aCxqyR5nVsHo/CvFtDr/cVMIPKVm44ufnZgj3VlED2gHp
+    nn1apfcVhz53NgBlXuBNh+TpiUZ6cDQ/OECwh6b18ssoqZ2iPmuLSozwcKd6EgOKTsG3cI
+    f3+v5e90ZfxTvZNjeFM+udYFTbi+iMT+/bfRzzE7GJE51xht1q4zzGec2u7vwo7UPg13dZ
+    0Rsam5pcm7W16KD29kz8m7NPEd706bF8fsdx4KQZ3E4lgRpJelpv19My8hpLBhv/bIJ0g9
+    3eJ/Xf6hIPFjPjabcOL0I8yOKSNuHdLAAJVazN4ag6uyrIk9k4HqYsHZ0LjLS9iNOQs474
+    kMwVZa9JUpKLovOjzpUZATJo6X88XhiIQjGoKQf9AZJBdJKjViQRNTlRBIyVAKFfPJeUso
+    bzlejwGpd9RoLSsO6so90AAAADAQABAAACACihtOFvN8YvpcPJU4d8jHE+L45XYd+pr/8B
+    6Vm7kzXcqAbpjsjXximLc0FMPUGgF0o079rG7taD871laZXgxXTA8YGrarWfGCINALVO89
+    hSzDiDd0wGj3Q8r/O/1/XewjfVL/gM3XyPdtXCJxuZ8Jz9LTv1Q5JUddSPI5+fWPGkyuYM
+    p/gNLcX5cOVBIwFBP2FFEc4/wDe5lKlrFEWSXNrTYjGbcD+KZptOAN4FLp53BipU7Zv/em
+    0TrxBjqYiNpAKPBrsciJuixCkJZPNSbs/HUXYpHHKDzxuk8BvVOpWO93XDV2dGIwNYkROn
+    SAR6NXfHkPe7ssj2wWnL4Pw10qjqYz4oLVwAyHi7JHRQ0VdhIJtOf7nkJNjwMlJhPlXUtt
+    oG/7fZREVbTtKl2oIK9z4KRSdjK8qNMpFZc9psvJEqrqLHWLrwLBPFiCZLci83jvZBD7Zl
+    5H3BVNBXwEEDyHTYDo2Eu15tBLPO3aInGURzDxLhA6YeYIBO8YRrLDxsxJ0B8yv3IL15vi
+    KDZeEjWWBD64W52Yra33bT8Dqd3S0Ub75cOX/L8aWbAJHu2XXnbyGE896Jk3+8RMnZYOEf
+    QDG1SKAPzdEeD1BvIl5/6vIIV8DtQwAXfdOrbJy1cEP++cP2u8UkfIMIULvhUzBC62Wsq+
+    /90YN5x3rSRo8qanA1AAABAQCJmw+hsHLPWl8t9AHE50v8OGRUzAv6oum+gb3fAYeIfDuZ
+    forISFosAvXLFCGolZinrgSEmJkDgzLS4fMdfoL4uxPr8TkBMiXfc7c5rs312xe1VzHBQx
+    ynM5JYOP6x9K61G8XA7zItyxl/julLL9qx1PHf5yR4wRvJMTiDCSlf9lc/lSp++2YxwiNz
+    Tj4S/0/2ArQcFc7YHLFyw5OEibfsdB1g3C7OcwMTi1g6lTZbAXA5ZvfmbgLASH6wMef/1x
+    xUE+/O9H4A6IjxknMZHUTnOBm+b0a6LJlMLCN2Ei78N+9z21d1Nj6D2iFjIEQqNhlrrnAD
+    iOfXIURtohDShed1AAABAQDYF5uxIE1u5WC9XnTVBMPuf376LOfA4WPhHuiQSdiCUxghqE
+    qJvzPmQDhcYCIhEV0wvxu9N2sY1w6sLn5j8w75G+OoYY3wD85yGv8YWkWz6bpZbiwZuvp/
+    xgyB/6I1P+InFpzwcQ0H48mq38ejNDH+2DitXQgAo+bQdIELc73ymLp18j488by54Cjs8I
+    y0tEaQwhOVQREeVLbOzqZcRV2poiWceZs6SE76Cb2NJtJcpYgcFYLkRheCMQlbnDHGsqXC
+    XCL962uCPX2xdICCoRCMcMruM0HrlZsC7iNZI2fgxSMGF3yjzSScaZAlKJ4MOObrzN3dUm
+    KzTo0j4YlNVQ0XAAABAQDHBPXNFowhrC/+r64JVdknj+5HX4qVHzIQ/TIZUfWUnNCt4atp
+    qSItjtFNY+VvPn2BHjyD2V0tPgwVkDWeWrMDhzY5fFoMqSslYN9PWFN0if0O1ig3tI3GUo
+    HBeHil3XlnG3na3lsDK+viFseirPc8fxeq0fOA70q1q+OqS0x+YANJJsfO/OGelHtFddtJ
+    IEnLSJZv2g8Y97Fg1Sz4Io4U0bPyzvxKv4H3gW8vqzcdoNyIKGtvsVpbElW3Fn8X+EEvgl
+    yNcN1oDsn6SeOBnZlLh2pzMAsSBWO75GgQ4xwZL5lCpCReH/XCmNbMCy5xVGOMRE/Y0J6g
+    nCZG53D4OLcrAAAAE3FhLWJvdEBzY3lsbGFkYi5jb20BAgMEBQYH
+    -----END OPENSSH PRIVATE KEY-----
+    """)
 
 
 def _return_response(body):
