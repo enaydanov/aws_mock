89c8420c28d234b7ffc47c1ce12b75f499371172
Bentsi Magidovich <bentsi@scylladb.com>
2021-10-25 13:50:12 +0300

    fix(run.sh): map 443 port

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ docker run -it --rm \
   --mount type=bind,source="${ROOT}/cert.key",target=/etc/nginx/cert.key,readonly \
   --mount type=bind,source="${ROOT}/cert.crt",target=/etc/nginx/cert.crt,readonly \
   --mount type=bind,source="${ROOT}/aws_mock",target=/src/aws_mock \
+  -p 443:443 \
   aws_mock
