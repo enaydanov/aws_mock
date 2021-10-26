e484dbc25f6470b7d09243a87fd426d94323abbc
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2021-10-26 07:17:15 +0000

    improvement(patch_etc_hosts.sh): delete or replace previously added hosts

--- scripts/patch_etc_hosts.sh
+++ scripts/patch_etc_hosts.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-set -xe
+set -e
 
-cat <<EOF >> /etc/hosts
+echo ">>> Remove previously added hosts from /etc/hosts:"
+sed -ni '/^### ADDED BY AWS MOCK TOOL ###$/,$!{p;b};w /dev/stdout' /etc/hosts
+echo
 
+if [[ "x$1" == "x--delete" ]]; then
+    exit 0
+fi
+
+AWS_MOCK_IP=${1:-127.0.0.1}
+
+echo ">>> Add hosts to /etc/hosts:"
+cat <<EOF | tee --append /etc/hosts
 ### ADDED BY AWS MOCK TOOL ###
-127.0.0.1   ec2.eu-north-1.amazonaws.com
-127.0.0.1   scylla-qa-keystore.s3.eu-west-1.amazonaws.com
-127.0.0.1   ec2.eu-west-2.amazonaws.com
+${AWS_MOCK_IP}  ec2.eu-north-1.amazonaws.com
+${AWS_MOCK_IP}  ec2.eu-west-2.amazonaws.com
+${AWS_MOCK_IP}  scylla-qa-keystore.amazonaws.com
+${AWS_MOCK_IP}  scylla-qa-keystore.s3.eu-north-1.amazonaws.com
+${AWS_MOCK_IP}  scylla-qa-keystore.s3.eu-west-1.amazonaws.com
 EOF
