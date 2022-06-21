9a72035b777eb3e7f2e9c105008eaf1928f2a5a9
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2022-06-21 14:58:34 +0000

    fix(install-ca.sh): fix problem with odd line numbers

--- aws_mock/templates/admin/install-ca.sh
+++ aws_mock/templates/admin/install-ca.sh
@@ -3,17 +3,16 @@
 set -xe
 
 AWS_MOCK_CA_CERT=/usr/local/share/ca-certificates/aws_mock.crt
-CERTIFI_CA_BUNDLE=$(python -c "import pathlib as p, certifi as c; print(p.Path(c.__file__).with_name('cacert.pem'))")
+CERTIFI_CA_BUNDLE=$(python -m certifi)
 
 echo ">>> Following lines removed from $CERTIFI_CA_BUNDLE:"
-sudo sed -ni 'N;/^\n# Issuer: CN=AWS Mock CA$/,/-----END CERTIFICATE-----/!{p;b};w /dev/stdout' "$CERTIFI_CA_BUNDLE"
+sudo sed -i '/^# Issuer: CN=AWS Mock CA$/,/^-----END CERTIFICATE-----$/{w /dev/stdout'$'\n'';d}' "$CERTIFI_CA_BUNDLE"
 echo
 
 cat << EOF | sudo tee "$AWS_MOCK_CA_CERT" | sudo tee -a "$CERTIFI_CA_BUNDLE"
-
 # Issuer: CN=AWS Mock CA
 # Subject: CN=AWS Mock CA
-{{ certificate }}
+{{ certificate -}}
 EOF
 
 sudo update-ca-certificates --fresh
